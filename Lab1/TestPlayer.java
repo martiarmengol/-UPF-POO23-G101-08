dd9556fb5abb9cacd6875fd3e52ef3ddd0029c0f
martiarmengol <marti.armengol01@estudiant.upf.edu>
2023-10-10 12:34:55 +0200

    Create TestPlayer.java

---
+++ Lab1/TestPlayer.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[]args){
+        Country C1 = new Country ("Spain");
+        Country C2 = new Country ("Sweden");
+        Country C3 = new Country ("Germany");
+        Country C4 = new Country ("USA");
+        
+
+    }
+}
