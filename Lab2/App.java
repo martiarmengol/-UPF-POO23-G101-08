b7e9cf216845cb28132eb425edabe1ca8f145848
martiarmengol <marti.armengol01@estudiant.upf.edu>
2023-10-25 15:54:13 +0200

    Update App.java

--- Lab2/App.java
+++ Lab2/App.java
@@ -19,7 +19,7 @@ public class App {
 
         String[] playerNames = { "Player 1", "Player 2", "Player 3", "Player 4", "Player 5", "Player 6", "Player 7", "Player 8", "Player 9", "Player 10" };
         int[] playerAges = { 25, 23, 27, 22, 24, 26, 28, 29, 30, 21 };
-        boolean[] playerGenders = { false, true, false, true, false, true, false, true, false, true };
+        Gender[] playerGenders = { Gender.MALE, Gender.FEMALE, Gender.MALE, Gender.FEMALE, Gender.MIXED, Gender.MIXED, Gender.MIXED, Gender.FEMALE, Gender.MALE, Gender.MIXED };
 
         for (int i = 0; i < playerNames.length; i++) {
             Player player = new Player(playerGenders[i], playerNames[i], playerAges[i], i % 5 == 0 ? countryA
@@ -55,6 +55,7 @@ public class App {
         league.printMatches();
 
         // Print team statistics
+        System.out.println("Nigger");
         for (Team team : league.getTeams()) {
             team.printStats();
         }
