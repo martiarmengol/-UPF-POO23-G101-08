b11fc0004fb4957a20a7421415365af42f244eda
martiarmengol <marti.armengol01@estudiant.upf.edu>
2023-10-25 12:42:53 +0200

    Update Match.java

--- Lab2/Match.java
+++ Lab2/Match.java
@@ -8,9 +8,8 @@ public class Match {
     private Team awayTeam;
     private int awayGoals;
     private int homeGoals;
-    
-    LinkedList<Player> homeScorers = new LinkedList<>();
-    LinkedList<Player> awayScorers = new LinkedList<>();
+    private LinkedList<Player> homeScorers = new LinkedList<>();
+    private LinkedList<Player> awayScorers = new LinkedList<>();
 
     public Match(Team hT, Team aT){
         homeTeam = hT;
@@ -25,6 +24,22 @@ public class Match {
         return homeGoals;
     }
 
+    public Team getHomeTeam(){
+        return homeTeam;
+    }
+
+    public Team getAwayTeam(){
+        return awayTeam;
+    }
+
+    public LinkedList<Player> getHomeScorers() {
+        return homeScorers;
+    }
+
+    public LinkedList<Player> getAwayScorers() {
+        return awayScorers;
+    }
+
     public void simulateMatch(){
         int n = 6;
         Random random = new Random();
@@ -32,17 +47,17 @@ public class Match {
         int awayGoals = random.nextInt(n);
 
         for(int i=0;i<homeGoals;i++){
-            int indexHome = random.nextInt(homeTeam.playerList.size());
-            homeScorers.add(homeTeam.playerList.get(indexHome));
-            int g = homeTeam.playerList.get(indexHome).getGoals();
-            homeTeam.playerList.get(indexHome).update_goals(g+1);
+            int indexHome = random.nextInt(homeTeam.getPlayers().size());
+            homeScorers.add(homeTeam.getPlayers().get(indexHome));
+            int g = homeTeam.getPlayers().get(indexHome).getGoals();
+            homeTeam.getPlayers().get(indexHome).update_goals(g+1);
         }
         
         for(int i=0;i<awayGoals;i++){
-            int indexAway = random.nextInt(awayTeam.playerList.size());
-            awayScorers.add(awayTeam.playerList.get(indexAway));
-            int g = awayTeam.playerList.get(indexAway).getGoals();
-            awayTeam.playerList.get(indexAway).update_goals(g+1);
+            int indexAway = random.nextInt(awayTeam.getPlayers().size());
+            awayScorers.add(awayTeam.getPlayers().get(indexAway));
+            int g = awayTeam.getPlayers().get(indexAway).getGoals();
+            awayTeam.getPlayers().get(indexAway).update_goals(g+1);
         }
 
     }
