84037058a7cee906a53daff34c4796911e4944df
martiarmengol <marti.armengol01@estudiant.upf.edu>
2023-10-26 12:05:57 +0200

    Update Player.java

--- Lab2/Player.java
+++ Lab2/Player.java
@@ -50,16 +50,16 @@ public class Player {
     public void update(Match m) {
         Random random = new Random();
     
-        // Update goals based on the match
+        // Update goals:
         int homeGoals = m.getHomeGoals();
         int awayGoals = m.getAwayGoals();
         this.noGoals += m.getHomeScorers().contains(this) ? homeGoals : awayGoals;
     
         // Set random values for other statistics:
-        this.noTackles = random.nextInt(11); // Random number of tackles (between 0 and 10)
-        this.noPasses = random.nextInt(51);  // Random number of passes (between 0 and 50)
-        this.noShots = random.nextInt(6);   // Random number of shots (between 0 and 5)
-        this.noAssists = random.nextInt(11); // Random number of assists (between 0 and 10)
+        this.noTackles = random.nextInt(11); 
+        this.noPasses = random.nextInt(51);  
+        this.noShots = random.nextInt(6);   
+        this.noAssists = random.nextInt(11); 
     }
     
 }
