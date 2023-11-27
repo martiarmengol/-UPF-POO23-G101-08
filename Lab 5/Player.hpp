babbcb5f715e28c7155ab9c33d3cfe9dcd53a46e
martiarmengol <marti.armengol01@estudiant.upf.edu>
2023-11-27 19:42:48 +0100

    2nd

---
+++ Lab 5/Player.hpp
@@ -0,0 +1,57 @@
+#ifndef _PLAYER_
+#define _PLAYER_
+
+#include <stdio.h>
+#include <string.h>
+#include "Country.hpp"
+class Match;
+
+virtual class Player{
+
+protected:
+    bool female;
+    std::string name;
+    int age,noMatches;
+    Country* nationality;
+    
+public:
+    Player(std::string name, bool g, int a, Country* nat) : name(name), female(g), age(a), nationality(nat), noMatches(0) {}
+    /*Player(string name, bool g,int a, Coutry nat){
+        this->name=name;
+        this->female=g;
+        this->age=a;
+        this->nationality=nat;
+        this->noMatches=0;
+    }*/
+
+    bool isFemale(){
+        return this->female;
+    }
+
+    int getNoMatches(){
+        return this->noMatches;
+    }
+
+    int getAge(){
+        return this->age;
+    }
+
+    std::string getName(){
+        return this->name;
+    }
+
+    Coutry * getNationality(){
+        return this->nationality;
+    }
+
+    bool equals(Object o){
+        
+    }
+
+    virtual void updateStats(Match * m)=0;
+    virtual void printStats()=0;
+    
+
+};
+
+#endif
--- Lab 5/Team.hpp
+++ Lab 5/Team.hpp
@@ -1,4 +1,3 @@
-
 #ifndef _TEAM_
 #define _TEAM_
 
